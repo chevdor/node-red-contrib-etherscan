ee06f901939eca4f0ab6f79b48235571e1d8170f
Will <w.kopp@kigroup.de>
2017-10-06 22:14:07 +0200

    Add etherscan

---
+++ etherscan.js
@@ -0,0 +1,44 @@
+module.exports = function(RED) {
+
+    if (false) { // Test for nodes compatibilities
+        throw "Info : not compatible";
+    }
+
+    function NodeConstructor(config) {
+        RED.nodes.createNode(this, config);
+        this.etherscan = RED.nodes.getNode(config.apiconfig);
+        var node = this;
+
+        node.on('input', function(msg) {
+            node.send(msg);
+            // var call = null;
+            // if (node.etherscan) {
+            //     console.log(node.etherscan.api);
+            //     switch (config.method) {
+            //         case "ethsupply":
+            //             call = node.etherscan.api.stats.ethsupply();
+            //             break;
+            //         case "ethprice":
+            //             call = node.etherscan.api.stats.ethprice();
+            //             break;
+            //         default:
+            //             node.status({ fill: "red", shape: "dot", text: "Unknown method: " + config.method });
+            //             break;
+            //     }
+
+            //     call.then(function(data) {
+            //         msg.payload = {
+            //             method: config.method,
+            //             data: data
+            //         };
+
+            //         node.send(msg);
+            //     });
+            // }
+        });
+
+        node.on("close", function() {});
+    };
+
+    RED.nodes.registerType("Etherscan", NodeConstructor);
+}
