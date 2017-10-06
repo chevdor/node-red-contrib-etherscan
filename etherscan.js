796ff905dfb0b2200dd7fd1c42a8ed948a9f0dd9
chevdor <w.kopp@kigroup.de>
2017-10-06 23:26:50 +0200

    Basic implementation of a few functions

--- etherscan.js
+++ etherscan.js
@@ -10,37 +10,33 @@ module.exports = function(RED) {
         var node = this;
 
         node.on('input', function(msg) {
-            node.send(msg);
-            // var call = null;
-            // if (node.etherscan) {
-            //     console.log(node.etherscan.api);
-            //     switch (config.method) {
-            //         case "ethsupply":
-            //             call = node.etherscan.api.stats.ethsupply();
-            //             break;
-            //         case "ethprice":
-            //             call = node.etherscan.api.stats.ethprice();
-            //             break;
-            //         default:
-            //             node.status({ fill: "red", shape: "dot", text: "Unknown method: " + config.method });
-            //             break;
-            //     }
+            // node.send(msg);
+            var call = null;
+            if (node.etherscan) {
+                // console.log(node.etherscan.api);
+                switch (config.method) {
+                    case "ethsupply":
+                        call = node.etherscan.api.stats.ethsupply();
+                        break;
+                    case "ethprice":
+                        call = node.etherscan.api.stats.ethprice();
+                        break;
+                    default:
+                        node.status({ fill: "red", shape: "dot", text: "Unknown method: " + config.method });
+                        break;
+                }
 
-            //     call.then(function(data) {
-            //         msg.payload = {
-            //             method: config.method,
-            //             data: data
-            //         };
+                call.then(function(data) {
+                    msg.payload = {
+                        method: config.method,
+                        data: data
+                    };
 
-            //         node.send(msg);
-            //     });
-            // }
+                    node.send(msg);
+                });
+            }
         });
-
-        node.on("close", function() {});
-
-        
-        
+        node.on("close", function() {});   
     };
 
     RED.nodes.registerType("Etherscan", NodeConstructor);
